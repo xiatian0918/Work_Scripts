a146625ae28dae2a5ce5cb66682e6f439eeb04a0
HotXia <18810434724@163.com>
2019-04-28 16:53:45 +0800

    夏天 脚本上传

---
+++ 工作脚本Python/上传文件到共享存储.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+# author: xiatian
+
+from smb.SMBConnection import SMBConnection
+def get_script_file():
+    conn = SMBConnection('admin', 'password', 'HotXia-PC', '\\192.168.0.29\Toprs-rd3-60T')
+    print(conn.connect('192.168.0.29', 20))
+    conn.listPath('\\192.168.0.29\\Toprs-rd3-60T')
+    # sharelist = conn.listShares()#列出共享目录
+    # for i in sharelist:
+    #     print (i)
