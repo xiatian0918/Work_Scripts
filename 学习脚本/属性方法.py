e782146e7c468714f2ac8b210c04d245ea8a12ab
HotXia <18810434724@163.com>
2019-04-29 14:01:29 +0800

    夏天 脚本上传

---
+++ 学习脚本/属性方法.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+# author: xiatian
+
+class Dog(object):
+    '''这是描述类的方法'''
+
+print(Dog.__doc__)
+print(Dog.__module__)
+print(Dog.__class__)
+
+
+
+
+
+
+
--- 学习脚本/静态方法.py
+++ 学习脚本/静态方法.py
@@ -2,14 +2,20 @@
 #-*- coding:utf-8 -*-
 # author: xiatian
 
+
 class Dog(object):
 
     def __init__(self,name):
         self.name = name
 
-    @staticmethod  #实际上跟类没什么关系了
-    def eat(self,food):
-        print("%s is eating %s" %(self.name,food))
+#    @staticmethod  #实际上跟类没什么关系了
+    @classmethod
+    def eat(self):
+        print("%s is eating %s" %(self.name,'asdasd'))
+
+    def talk(self):
+        print("%s is talking" %self.name)
 
 d = Dog("ChenRongHua")
-d.eat()
+d.eat(d)
+d.talk()
