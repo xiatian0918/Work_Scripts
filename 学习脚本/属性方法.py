317dcb6eb921594beb1b3ca33c6669500954a32e
HotXia <18810434724@163.com>
2019-05-05 08:39:02 +0800

    夏天 脚本上传

--- 学习脚本/属性方法.py
+++ 学习脚本/属性方法.py
@@ -2,12 +2,50 @@
 #-*- coding:utf-8 -*-
 # author: xiatian
 
-class Dog(object):
-    '''这是描述类的方法'''
+# class Dog(object):
+#     '''这是描述类的方法'''
+#
+#     def __call__(self, *args, **kwargs):
+#         print("runing call")
+#
+# print(Dog.__doc__)
+# print(Dog.__module__)
+# print(Dog.__class__)
+# print(Dog.__dict__)   #打印类里的所有属性，不包括示例属性
+#
+# d = Dog()
+# d()
 
-print(Dog.__doc__)
-print(Dog.__module__)
-print(Dog.__class__)
+# class Foo(object):
+#
+#     def __init__(self,name):
+#         self.name = name
+#
+#     def talk(self):
+#         print("hehe")
+#
+# f = Foo('alex')
+#
+# print(type(f))
+# print(type(Foo))
+
+def func(self):
+    print('hello %s' % self.name)
+
+def __init__(self,name,age):
+    self.name = name
+    self.age = age
+
+Foo = type('Foo', (object,), {'func':func,
+                       '__init__':__init__})
+
+f = Foo('chen',34)
+f.func()
+print(type(Foo))
+
+
+
+
 
 
 
