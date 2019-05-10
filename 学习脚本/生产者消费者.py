c4adb0ee15eac1ccf64c8fe3b98a69e01466a4af
HotXia <18810434724@163.com>
2019-05-10 14:17:56 +0800

    夏天 脚本更新 20190510

--- 学习脚本/生产者消费者.py
+++ 学习脚本/生产者消费者.py
@@ -3,15 +3,27 @@
 # author: xiatian
 
 import threading,time,queue
-q = queue.Queue()
+
+q = queue.Queue(maxsize=10)
 def Producer(name):
-    for i in range(10):
-        q.put("骨头%s" %i)
+    count = 1
+    while True:
+        q.put("骨头%s" %count)
+        print("生产了骨头" ,count)
+        count +=1
+        time.sleep(0.1)
 
 def Consumer(name):
-    while q.qsize()>0:
+#    while q.qsize()>0:
+    while True:
         print("[%s] 取到[%s] 并且吃了它..." %(name,q.get()))
+        time.sleep(1)
 
 
 p = threading.Thread(target=Producer,args=("Alex",))
 c = threading.Thread(target=Consumer,args=("hahha",))
+z = threading.Thread(target=Consumer,args=("xiatian",))
+
+p.start()
+c.start()
+z.start()
