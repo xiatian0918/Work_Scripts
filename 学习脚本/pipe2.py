9cdcee5a705216309bbc0d038cecbb98f50f3190
HotXia <18810434724@163.com>
2019-05-10 16:09:07 +0800

    夏天 脚本更新 20190510

---
+++ 学习脚本/pipe2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+# author: xiatian
+
+from multiprocessing import Process, Pipe
+
+def f(conn):
+    conn.send([42, None, 'hello from child1'])
+    conn.send([42, None, 'hello from child2'])
+    print("from parent:", conn.recv())
+    conn.close()
+
+
+if __name__ == '__main__':
+    parent_conn, child_conn = Pipe()
