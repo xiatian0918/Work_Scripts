0c5867b7bcb2728aa37a998be1fbab09da800cb0
HotXia <18810434724@163.com>
2019-05-10 17:29:48 +0800

    夏天 脚本更新 20190510

--- 学习脚本/进程池.py
+++ 学习脚本/进程池.py
@@ -2,7 +2,6 @@
 #-*- coding:utf-8 -*-
 # author: xiatian
 
-
 from multiprocessing import Process,Pool,freeze_support
 import os
 import time
@@ -12,17 +11,19 @@ def Foo(i):
     print("in process",os.getpid())
     return i+100
 
-
 def  Bar(arg):
-    print('---->exec done:', arg)
+    print('---->exec done:', arg,os.getpid())
 
 if __name__ == '__main__':
     freeze_support()
-    pool = Pool(5)
+#    pool = Pool(5)
+    pool = Pool(processes=3)   #允许进程池同时放入5个进程
+    print("主进程",os.getpid())
 
     for i in range(10):
-        pool.apply(func=Foo,args=(i,))
-
+ #       pool.apply(func=Foo,args=(i,))     #串行
+ #       pool.apply_async(func=Foo, args=(i,))
+        pool.apply_async(func=Foo, args=(i,), callback=Bar)
     print('end')
     pool.close()
-    #pool.join()
+    pool.join()
