026c1f919448723ba5d016c90e4276a38a384e24
HotXia <18810434724@163.com>
2019-05-10 14:59:02 +0800

    夏天 脚本更新 20190510

---
+++ 学习脚本/线程.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+# author: xiatian
+
+import threading
+
+def run(name):
+    print("%s run thread...." %name)
+
+t_res = []
+for i in range(10):
+    t = threading.Thread(target=run,args=('xiatian',))
+    t.start()
+    t_res.append(t)
+
+for r in t_res:
+    r.join()
