c2185501dcc9ea5524f62860b14125fb95f3bec4
HotXia <18810434724@163.com>
2019-05-10 15:55:07 +0800

    夏天 脚本更新 20190510

--- 学习脚本/进程queue.py
+++ 学习脚本/进程queue.py
@@ -2,6 +2,7 @@
 #-*- coding:utf-8 -*-
 # author: xiatian
 
+
 from multiprocessing import Queue,Process
 
 import queue,threading
@@ -9,13 +10,13 @@ import queue,threading
 # def f(q):
 #     q.put([42, None, 'hello'])
 
-def f():
-    q.put([42, None, 'hello'])
+def f(qq):
+    qq.put([42, None, 'hello'])
 
 if __name__ == '__main__':
-    q = queue.Queue()
-    p = Process(target=f, )
-#    p = threading.Thread(target=f, )
+    q = Queue()
+    p = Process(target=f, args=(q,))
+   # p = threading.Thread(target=f, )
     p.start()
-    print(q.get())
+    print(q.get())  #print "[42, None, 'hello']"
     p.join()
