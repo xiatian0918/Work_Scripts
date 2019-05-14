095ce99069df4c45992d2d1076d64ac280e2d925
HotXia <18810434724@163.com>
2019-05-14 11:47:18 +0800

    夏天 脚本更新 20190514

--- 工作脚本Python/查看端口被占用及通过pid杀掉进程.py
+++ 工作脚本Python/查看端口被占用及通过pid杀掉进程.py
@@ -4,6 +4,7 @@
 # 获取端口进程脚本
 
 import os
+
 win_port = input("请输入您想要查看被占用的端口号: ")
 info_port = os.popen("netstat -ano|findstr %s" %win_port).read()
 # print(info_port)
@@ -13,3 +14,8 @@ name_pid = pid_port[0].split(' ')[-1]
 
 serv_pid = os.popen("tasklist|findstr %s" %name_pid).read()
 print(serv_pid)
+
+# try:
+#     import hotxia
+# except Exception as e:
+#     pass
