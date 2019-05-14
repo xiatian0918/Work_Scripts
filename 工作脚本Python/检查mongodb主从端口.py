292962dbb2bb7bfde82969ff549706251c1d6f4b
HotXia <18810434724@163.com>
2019-05-14 09:30:05 +0800

    夏天 脚本更新 20190514

--- 工作脚本Python/检查mongodb主从端口.py
+++ 工作脚本Python/检查mongodb主从端口.py
@@ -3,22 +3,15 @@
 # author: xiatian
 
 import paramiko
-
 shell = "ssh 192.168.0.59 '/bin/sh /data/scripts/check_mongo_rep.sh'"
-
 def sshclient_execmd(hostname, port, username, password, execmd):
-    #paramiko.util.log_to_file("paramiko.log")
-
     s = paramiko.SSHClient()
     s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
     s.connect(hostname=hostname, port=port, username=username, password=password)
     stdin, stdout, stderr = s.exec_command(execmd)
-    stdin.write("Y")  # Generally speaking, the first connection, need a simple interaction.
+    stdin.write("Y")
     print(stdout.read().decode())
-
     s.close()
-
 
 def main():
     hostname = '192.168.0.62'
@@ -26,10 +19,7 @@ def main():
     username = 'root'
     password = 'Toprs!@#123'
     execmd = shell
-
-
     sshclient_execmd(hostname, port, username, password, execmd)
-
 
 if __name__ == "__main__":
     main()
