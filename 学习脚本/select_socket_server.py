cacbf88ccddf078c712c7ae8d55695d2ec53641e
HotXia <18810434724@163.com>
2019-05-15 09:48:22 +0800

    夏天 脚本更新 20190515

--- 学习脚本/select_socket_server.py
+++ 学习脚本/select_socket_server.py
@@ -15,5 +15,11 @@ outputs = []
 
 readable , writeable, exceptional = select.select(inputs, outputs, inputs)
 print(readable,writeable,exceptional)
-
-server.accept()
+for i in readable:
+    if r is server: #代表来了一个新链接
+        conn,addr = server.accept()
+        print("来了个新链接",addr)
+        inputs.append(conn)
+    else:
+        data = conn.recv(1024)
+        print("收到数据",data)
