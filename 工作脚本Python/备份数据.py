71409b21f0f8647a097964377d7f46ade377d405
HotXia <18810434724@163.com>
2019-05-30 10:48:00 +0800

    夏天 脚本更新 20190530

--- 工作脚本Python/备份数据.py
+++ 工作脚本Python/备份数据.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 # author: xiatian
+# 这个脚本是把保密检查出来的文件自动保存到共享文件系统中
+
+import shutil
 
 import paramiko,os
-your_name=input("请输入你的名字：")
+your_name=input("请输入要备份的用户名字：")
 
-shell = "/bin/sh /data/scripts/modify_secret.sh %s " %your_name
+shell = "/bin/sh /data/scripts/bak_file.sh %s " %your_name
 
 def sshclient_execmd(hostname, port, username, password, execmd):
 
@@ -14,17 +17,23 @@ def sshclient_execmd(hostname, port, username, password, execmd):
 
     s.connect(hostname=hostname, port=port, username=username, password=password)
     stdin, stdout, stderr = s.exec_command(execmd)
-    stdin.write("Y")  # Generally speaking, the first connection, need a simple interaction.
+    stdin.write("Y")
 
     print(stdout.read().decode())
 
     s.close()
 
-
 def bak_data():
-    with open('Z:\\system\\check\\%s_结果.txt' %your_name, 'r',encoding='utf-8') as f:
+    with open('Z:\\monitor_data\\bak_bm_file\\%s_结果.txt' %your_name, 'r+',encoding='utf-8') as f:
         for i in f:
-            print(i)
+
+            # resault = os.popen(" COPY %s  Z:\\monitor_data\\bak_bm_file\\%s\\ " %(i,your_name))
+            # print(resault.read())
+            if i:
+                i=i.replace('\n','')
+                name=i.split('\\')[-1]
+                print(name)
+                shutil.copyfile(i, 'Z:\\monitor_data\\bak_bm_file\\%s\\\%s' %(your_name,name))
 
 
 def main():
@@ -34,7 +43,12 @@ def main():
     password = 'Toprs!@#123'
     execmd = shell
 
+
     sshclient_execmd(hostname, port, username, password, execmd)
 
 if __name__ == "__main__":
     main()
+
+    bak_data()
+
+
