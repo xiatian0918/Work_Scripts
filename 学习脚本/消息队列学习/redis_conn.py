e55e90be4188693a0d5489e645187010b730c530
HotXia <18810434724@163.com>
2019-07-17 10:07:00 +0800

    夏天 脚本更新 20190717

--- 学习脚本/消息队列学习/redis_conn.py
+++ 学习脚本/消息队列学习/redis_conn.py
@@ -2,10 +2,25 @@
 #-*- coding:utf-8 -*-
 # author: xiatian
 
-import redis
+import redis,time
+
+# r = redis.Redis(host='192.168.56.11', port=6380)
+#
+# r.set('foo', 'Bar')
+# print (r.get('foo'))
 
 
-r = redis.Redis(host='192.168.56.11', port=6380)
+pool = redis.ConnectionPool(host='192.168.56.11', port=6380,db=5)
 
-r.set('foo', 'Bar')
-print (r.get('foo'))
+r = redis.Redis(connection_pool=pool)
+
+# pipe = r.pipeline(transaction=False)
+pipe = r.pipeline(transaction=True)
+
+r.brpoplpush('names','name2',timeout=30)
+r.set('name7','wupeiqi')
+# r.set('name', 'alex')
+# #time.sleep(20)
+# r.set('role', 'sb')
+#
+pipe.execute()
