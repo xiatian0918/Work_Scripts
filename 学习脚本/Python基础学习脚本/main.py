05289ae9d8ad8814eff26e94d0f0e11260bcab30
HotXia <18810434724@163.com>
2019-07-17 11:02:52 +0800

    夏天 脚本更新 20190717

--- 学习脚本/Python基础学习脚本/main.py
+++ 学习脚本/Python基础学习脚本/main.py
@@ -6,11 +6,11 @@
 #导入指定路径模块
 
 def main():
-    from auto_scripts.学习脚本 import module_test
+    from auto_scripts.学习脚本.Python基础学习脚本 import module_test
     print(module_test.say_hello())
     module_test.logger()
 
-    from auto_scripts.学习脚本.module_test import name
+    from auto_scripts.学习脚本.Python基础学习脚本.module_test import name
     print(name)
 
 if __name__ == '__main__':
