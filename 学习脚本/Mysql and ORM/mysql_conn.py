9a80478cf8c48c3fa85f08c38a68ebebe93172fe
HotXia <18810434724@163.com>
2019-07-18 08:44:02 +0800

    夏天 脚本更新 20190718

--- 学习脚本/Mysql and ORM/mysql_conn.py
+++ 学习脚本/Mysql and ORM/mysql_conn.py
@@ -10,8 +10,21 @@ conn = pymysql.connect(host='192.168.56.11', port=3306, user='root', passwd='xia
 cursor = conn.cursor()
 
 # 执行SQL，并返回收影响行数
-effect_row = cursor.execute("select * from student")
-print(cursor.fetchone()) #读一行内容
-# print(cursor.fetchone())
-print('------------------------->')
-print(cursor.fetchall())  # 读所有行
+# effect_row = cursor.execute("select * from student")
+# print(cursor.fetchone()) #读一行内容
+# # print(cursor.fetchone())
+# print('------------------------->')
+# print(cursor.fetchall())  # 读所有行
+
+
+date = [
+    ("N1","2015-05-22",'M',21),
+    ("N2","2015-05-23",'M',22),
+    ("N3","2015-05-24",'F',23),
+]
+
+cursor.executemany("insert into student (name,register_date,gender,age) values (%s,%s,%s,%s)",date)
+
+conn.commit()  # 默认创建事务，因此要提交
+
+
