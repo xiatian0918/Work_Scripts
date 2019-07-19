2059d6db0db6127a4e95545149c7f6a9ebf587a1
HotXia <18810434724@163.com>
2019-07-19 14:32:19 +0800

    夏天 脚本更新 20190719

--- 学习脚本/堡垒机开发/fuckJumpServer/modules/models.py
+++ 学习脚本/堡垒机开发/fuckJumpServer/modules/models.py
@@ -2,22 +2,46 @@
 #-*- coding:utf-8 -*-
 # author: xiatian
 
-from sqlalchemy import Table, Column, Integer,String,DATE, ForeignKey,engine
+from sqlalchemy import Table, Column, Enum,Integer,String,DATE, ForeignKey,engine
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import ChoiceType,PasswordType
 
 Base = declarative_base()
 
 class Host(Base):
-    pass
+    __tablename__ = 'host'
+    id = Column(Integer,primary_key=True)
+    hostname = Column(String(64),unique=True)
+    ip = Column(String(64),unique=True)
+    port = Column(Integer,default=22)
+
+    def __repr__(self):
+        return self.hostname
 
 class HostGroup(Base):
-    pass
+    __tablename__ = 'host_group'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(64), unique=True)
+
+    def __repr__(self):
+        return self.name
 
 class RemoteUser(Base):
-    pass
+    __tablename__ = 'remote_user'
+    id = Column(Integer, primary_key=True)
+    AuthTypes = [
+        ('ssh-passwd','SSH/Password'),
+        ('ssh-key','SSH/KEY'),
+    ]
+    auth_type = Column(ChoiceType(AuthTypes))
+    username = Column(String(32))
+    password = Column(String(128))
+
+    def __repr__(self):
+        return self.username
 
 class UserProfile(Base):
     pass
