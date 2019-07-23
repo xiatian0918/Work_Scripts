f4a29010da48c894ad126a3b2080d813e3e1d3df
HotXia <18810434724@163.com>
2019-07-23 11:37:56 +0800

    夏天 脚本更新 20190723

--- 学习脚本/堡垒机开发/fuckJumpServer/conf/action_registers.py
+++ 学习脚本/堡垒机开发/fuckJumpServer/conf/action_registers.py
@@ -11,7 +11,7 @@ actions = {
     'create_groups': views.create_groups,
     'create_hosts': views.create_hosts,
     'create_bindhosts': views.create_bindhosts,
-    'create_remoteusers': views.create_remoteusers,
-    'audit':views.log_audit
+    'create_remoteusers': views.create_remoteusers
+ #   'audit':views.log_audit
 
 }
